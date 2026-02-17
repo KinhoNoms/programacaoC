37fb0edd7c71e0cb6beb62f7b1d5a80d6c761643
KinhoNoms <kinhonoms@gmail.com>
2026-02-17 22:31:47 +0000

    construido ate linha 20

---
+++ super_trunfo/super_trunfo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    char estado [10];
+    char codigo [4];
+    char nome [10];
+    int  populaçao;
+    float area;
+    float pib;
+    int  turismo;
+
+    printf("      DESAFIO SUPER TRUNFO \n");
+    printf("CARTA 1 \n");
+
+    printf("Digite o Estado descrito na sua carta: \n");
+    scanf("%s", estado);
+    
+    printf("Digite o Código da sua carta: \n ");
+    scanf("%s", codigo);
+
+    
+
+
+
+
+
+
+    return 0;
+}
+
